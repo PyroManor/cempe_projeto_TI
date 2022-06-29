2362ade074c0ace09b5f7a034f8454b69ade4fdd
Carlos Eduardo Sbardella Souza <68085868+PyroManor@users.noreply.github.com>
2022-06-29 08:31:25 -0300

    Add files via upload

---
+++ TI_Trab_Integrador/TI-CEMPE/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+  <head>
+    <meta charset='UTF-8'/>
+    <title>TELAS FRONT-END!</title>
+    <link rel='stylesheet' href='style.css'/>
+  </head>
+  <body>
+    <div class='header'>
+      
+      <ul class='menu'>
+        <li class='dropdown'><span></span></li>
+        <li><a href='#'>Blog</a></li>
+        <li><a href='#'>Assinar</a></li>
+        <li><a href='#'>Sobre</a></li>
+      </ul>
+    </div>
+    <div class='menu-container'>
+      <div class='menu'>
+        <h1>AS MELHORES FERRAMENTAS 
+      PARA EMPREENDEDORES</h1>
+    <p>Aqui lhe ajudaremos a fazer uma melhor estratégia mercadológica fazendo uso
+da metodologia Design Thinking (imersão, ideação e protótipos), além
+de dicas, sugestões, recomendações de softwares, sites, e-books/cursos.</p>
+
+ <style>
+.button {
+  display: inline-block;
+  border-radius: 30px;
+  background-color: #6c63ff;
+  border: none;
+  color: #FFFFFF;
+  text-align: center;
+  font-size: 15px;
+  font-style: normal;
+  font-family: "Montserrat", serif;
+  padding: 20px;
+  width: 200px;
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 5px;
+}
+
+.button span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.button span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.button:hover span {
+  padding-right: 25px;
+}
+
+.button:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+</style>
+</head>
+<body>
+  <a href="./screens/tela_login.php" button class="button"  style="vertical-align:middle" ><span>Entrar ou Registrar-se </span></a>
+
+    <img class="displayed" src='imagens/image1.svg' width='400'/>
+    <br>
+     
+<div style="float: left">
+<img class="revenue" src='imagens/image2.svg' width='200' height='200 class='article-image' />
+</div>
+<div>
+  <hr>
+  <h1>O que o site traz: </h1>
+<p>Ajuda e criatividade, sabendo como funciona os meios de propagandas entre as redes sociais e oq é mais atrativo para chamar a atenção do seu cliente alvo. </p> 
+    <br>
+</div>
+</div>
+
+    <br>
+  <img class="displayed" src='imagens/image3.svg' width='200' height='200'/>
+  <h2>Como se portar diante as redes sociais:</h2>
+   <p>Estaremos disponibilizando PDFs e e dicas de como se comportar em redes sociais a função de seu produto, como tirar as fotos, legendas, organização em um geral.</p> 
+    <br>
+  <img class="revenue" src='imagens/image4.svg' width='200' height='200'/>
+   <h3>Tudo o que precisará:</h3>
+  <p>O que você precisará, desde PDFs e links para os apps, estarão aqui no nosso site, você não vai precisar pesquisar e entrar em paginas uma por uma, pois nos disponibilizamos a nossos usuários tudo que aqui é dito, os links direto para as páginas na qual também terá instruções de como usar, será totalmente 100% gratuito.</p> 
+    <br>
+  </body>
+</html>
